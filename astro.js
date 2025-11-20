dc62f9ccd8dd4df34c3064e7a7747cb323b6b15e
Rowan Fortier <rowan.fortier@gmail.com>
2025-11-19 19:55:47 -0800

    add stars and animation to astro page

---
+++ astro.js
@@ -0,0 +1,22 @@
+const stars1 = document.getElementById("stars");
+const stars2 = document.getElementById("stars2");
+const stars3 = document.getElementById("stars3");
+
+
+
+function addStars(name) {
+
+    var starstring = "";
+
+    for (var i = 0; i < 100; i++) {
+        var random1 = Math.trunc(Math.random()*1000);
+        var random2 = Math.trunc(Math.random()*4000);
+        starstring += `${random1}px ${random2}px #FFF , `;
+    }
+    starstring = starstring.slice(0,-2);
+    name.style.boxShadow = starstring;
+}
+
+addStars(stars1);
+addStars(stars2);
+addStars(stars3);
