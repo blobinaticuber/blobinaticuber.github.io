6a69613d9ad44a79663c3f73defa485c2d73e5ac
Rowan Fortier <rowan.fortier@gmail.com>
2025-12-14 23:44:45 -0800

    Create cubie clicker page with animated bg

---
+++ cubieclicker.js
@@ -0,0 +1,62 @@
+
+const stickers1 = document.getElementById("stickers");
+
+const cubeColours = ["#FFFFFF", "#ffff00", "#FF0000", "#00FF00", "#0000FF", "#FFA500"];
+
+var stickersColours = [];
+var stickerX = [];
+var stickerY = [];
+var stickerXvelocity = [];
+var stickerYvelocity = [];
+
+
+function initializeBG() {
+    stickers1.style.boxShadow = "";
+
+    var stickerstring = "";
+
+    for (var i = 0; i < 30; i++) {
+        stickerX[i] = Math.trunc(Math.random()*(window.innerWidth-200))+100;
+        stickerY[i] = Math.trunc(Math.random()*(window.innerHeight-200))+100;
+        stickerXvelocity[i] = 3*Math.random()-0.5;
+        stickerYvelocity[i] = 3*Math.random()-0.5;
+        stickersColours[i] = cubeColours[Math.trunc(Math.random()*cubeColours.length)];
+        stickerstring += `${stickerX[i]}px ${stickerY[i]}px ${stickersColours[i]} , `;
+    }
+    stickerstring = stickerstring.slice(0,-2);
+    stickers1.style.boxShadow = stickerstring;
+}
+
+
+function updateStickers() {
+    stickers1.style.boxShadow = "";
+
+    console.log("updating stickers");
+
+    var stickerstring = "";
+
+    for (var i = 0; i < 30; i++) {
+
+        // invert velocity if goes off screen
+        if (stickerX[i] + stickerXvelocity[i] > window.innerWidth-50 || stickerX[i] + stickerXvelocity[i] < 0) {
+            stickerXvelocity[i] = -stickerXvelocity[i];
+        }
+        if (stickerY[i] + stickerYvelocity[i] > window.innerHeight-50 || stickerY[i] + stickerYvelocity[i] < 0) {
+            stickerYvelocity[i] = -stickerYvelocity[i];
+        }
+
+        stickerX[i] += stickerXvelocity[i];
+        stickerY[i] += stickerYvelocity[i];
+
+
+
+
+
+        stickerstring += `${stickerX[i]}px ${stickerY[i]}px ${stickersColours[i]} , `;
+    }
+    stickerstring = stickerstring.slice(0,-2);
+    stickers1.style.boxShadow = stickerstring;
+}
+
+initializeBG();
+setInterval(updateStickers, 33);
