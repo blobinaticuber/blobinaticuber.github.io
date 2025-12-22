87bc2d03ab63d1bbb2c83f75f6dcff7f5ddba22e
Rowan Fortier <rowan.fortier@gmail.com>
2025-12-21 21:44:53 -0800

    add timer and cubella

--- cubieclicker.js
+++ cubieclicker.js
@@ -92,6 +92,9 @@ function divClasses(cl) {
 
 
 
+var cubellaUnlocked = false;
+
+
 const totalCubes = 43252003274489856000n;
 var cubesSolved = 0n;
 
@@ -109,7 +112,7 @@ function startGame() {
     var subbody = document.getElementById("subbody");
 
 
-    subbody.appendChild(titleAndButtons());
+    // subbody.appendChild(titleAndButtons());
     subbody.appendChild(cubeCountDisplay());
     subbody.appendChild(magicPackage());
 }
@@ -118,7 +121,7 @@ function titleAndButtons() {
     var d3 = divClasses("flexbox space-between");
     var left = divClasses("flexbox center");
     left.appendChild(h1Tag("Cubie Clicker"));
-    left.appendChild(pCodeTag("v0.0.2"));
+    left.appendChild(pCodeTag("v0.0.3"));
     d3.appendChild(left);
 
     var right = divClasses("flexbox center");
@@ -153,19 +156,13 @@ function magicPackageClicked() {
     var subbody = document.getElementById("subbody");
     var pkg = document.getElementById("magicPackage");
     subbody.removeChild(pkg);
-    subbody.appendChild(cubeButton());
+    // subbody.appendChild(cubeButton());
     // don't let them click the cube yet because they don't know how to solve it
     // show the inventory of the package
     subbody.appendChild(packageInventory());
 }
 
-function packageInventory() {
-    var d = divClasses("frosted");
-    d.id = "packageInventory";
-    d.appendChild(pTag("Contents of Magical package"));
-
-    var d2 = divClasses("flexbox center");
-
+function instructionManual() {
     var d3 = divClasses("frosted");
     d3.appendChild(pTag("Instruction manual"));
     d3.id = "instructionManualContainer";
@@ -174,11 +171,54 @@ function packageInventory() {
     instructionManualImg.src = "images/cubieclicker/instructions.png";
     d3.appendChild(instructionManualImg);
     d3.appendChild(pTag("- Teaches you how to solve"));
+    return d3;
+}
 
-    d2.appendChild(d3);
+function CubeCreature() {
+    var d3 = divClasses("frosted");
+    d3.appendChild(pTag("Strange Creature"));
+    d3.id = "cubeCreatureContainer";
+    var cubellaImg = document.createElement("img");
+    cubellaImg.onclick = CubeCreatureClicked;
+    cubellaImg.src = "images/cubieclicker/cubella_0.png";
+    cubellaImg.width = 200;
+    cubellaImg.height = 200;
+    d3.appendChild(cubellaImg);
+    d3.appendChild(pTag("- ???????????????????"));
+    return d3;
+}
 
-        // d2.appendChild(pTag("Magical Crystal"));
-        // d2.appendChild(pTag("Instruction manual"));
+function CubeCreatureClicked() {
+    cubellaUnlocked = true;
+    document.getElementById("cubeCreatureContainer").remove();
+    if (canSolve) {
+        document.getElementById("packageInventory").remove();
+    }
+
+    var r = document.getElementById("rightsubbody");
+    r.appendChild(cubella());
+}
+
+// makes cubella UI on the right
+function cubella() {
+    var d3 = divClasses("frosted");
+    d3.appendChild(pTag("Cubella - Level 1"));
+    var cubellaImg = document.createElement("img");
+    cubellaImg.src = "images/cubieclicker/cubella_0.png";
+    cubellaImg.width = 200;
+    cubellaImg.height = 200;
+    d3.appendChild(cubellaImg);
+    return d3;
+}
+
+function packageInventory() {
+    var d = divClasses("frosted");
+    d.id = "packageInventory";
+    d.appendChild(pTag("Contents of Magical package"));
+
+    var d2 = divClasses("flexbox center");
+    d2.appendChild(instructionManual());
+    d2.appendChild(CubeCreature());
 
     d.appendChild(d2);
     return d;
@@ -187,8 +227,14 @@ function packageInventory() {
 function instructionManualClicked() {
     // remove manual from package contents
     document.getElementById("instructionManualContainer").remove();
+    // show the rubik's cube clicker button
+    document.getElementById("cubeCounter").after(cubeButton());
     // remove block on solve
     canSolve = true;
+
+    if (cubellaUnlocked) {
+        document.getElementById("packageInventory").remove();
+    }
 }
 
 // convert bigInt into readable number with commas
@@ -202,6 +248,7 @@ function bigIntFormat(bi) {
 function cubeCountDisplay() {
     var d = divClasses("frosted");
     d.appendChild(pTag("Rubik's Cubes solved"));
+    d.id = "cubeCounter";
 
     var d2 = divClasses("flexbox center");
 
@@ -219,8 +266,17 @@ function updateCubeCount() {
 }
 
 function cubeButton() {
-    var d = document.createElement("div");
-    d.classList = "flexbox center frosted";
+    var d = divClasses("center frosted");
+
+    var timerdiv = document.createElement("div");
+    timerdiv.style = "width:100%; height:24px; border-radius: 25px; border: 2px solid white;";
+    var timerprogress = document.createElement("div");
+    timerprogress.id = "timerBar";
+    timerprogress.style = "width:0%; height:24px; background-color: #1c9124ff; border-radius: 25px;";
+
+    timerdiv.appendChild(timerprogress);
+    d.appendChild(timerdiv);
+
     var cubeimg = document.createElement("div");
     cubeimg.id = "cubeimg";
     cubeimg.onclick = cubeClicked;
@@ -248,8 +304,28 @@ function update333Image() {
 
 function cubeClicked() {
     if (canSolve) {
-        cubesSolved = cubesSolved + 1n;
-        updateCubeCount();
-        update333Image();
+        // disable clicking on the cube during a solve
+        document.getElementById("cubeimg").onclick = null;
+
+        var tb = document.getElementById("timerBar");
+        var w = 0;
+        var id = setInterval(frame, 10);
+        // call this every 10 milliseconds (0.01) seconds
+        function frame() {
+            if (w >= 100) {
+            clearInterval(id);
+            cubesSolved = cubesSolved + 1n;
+            updateCubeCount();
+            update333Image();
+            document.getElementById("cubeimg").onclick = cubeClicked;
+            tb.style.width = 0 + '%';
+            } else {
+            w += 1/solveTime/60*100;
+            tb.style.width = w + '%';
+            }
+        }
+
+
+
     }
 }
