e5193293b8fe8fe2fbe7e73660bc3bd02ebdb1fb
Rowan Fortier <rowan.fortier@gmail.com>
2026-01-23 22:14:54 -0800

    Add beautiful win graphics

--- games/ttt/ttt.js
+++ games/ttt/ttt.js
@@ -7,6 +7,9 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FFFFFF";
 ctx.fillRect(0,0,canvas.clientWidth, canvas.clientHeight);
 
+var playerColors = ["red", "blue"];
+var highlightColors = ["#FF000044", "#0000FF44"];
+
 function rect(bcolor, x, y, w, h, strokeWidth) {
     ctx.beginPath();
     ctx.rect(x, y, w, h);
@@ -34,6 +37,13 @@ function drawGrid() {
 
 }
 
+function highlightCell(color, bx, by) {
+    ctx.fillStyle = color;
+    ctx.fillRect(BoardCoordtoCanvasCoord(bx),BoardCoordtoCanvasCoord(by),100, 100);
+}
+
+
+
 function drawWinnerText(txt) {
     ctx.fillStyle = "black";
     ctx.font = "50px Arial";
@@ -44,9 +54,9 @@ function drawWinnerText(txt) {
 function drawX(color, x, y) {
     // convert game board coords to canvas drawing coords
     x = BoardCoordtoCanvasCoord(x);
-    x += -50;
+    x += 50;
     y = BoardCoordtoCanvasCoord(y);
-    y += -50;
+    y += 50;
 
     ctx.beginPath();
 
@@ -69,9 +79,9 @@ function drawX(color, x, y) {
 function drawO(color, x, y) {
     // convert game board coords to canvas drawing coords
     x = BoardCoordtoCanvasCoord(x);
-    x += -50;
+    x += 50;
     y = BoardCoordtoCanvasCoord(y);
-    y += -50;
+    y += 50;
 
     ctx.beginPath();
 
@@ -99,7 +109,7 @@ function CanvasCoordtoBoardCoord(c) {
 function BoardCoordtoCanvasCoord(c) {
     c++;
     c*=100;
-    c+=100;
+    // c+=100;
     return c;
 }
 
@@ -123,33 +133,41 @@ const planeWins = [
 
 function findWin() {
     // searches for wins
+    var potentialWinCoords = [[[]]];
     for (w=0; w<planeWins.length; w++) {
         // linescore is the sum of pieces in a winning line (3 means X win, -3 means O win, anything else is not a win)
+        potentialWinCoords = planeWins[w];
         const linescore = board[planeWins[w][0][0]][planeWins[w][0][1]] + board[planeWins[w][1][0]][planeWins[w][1][1]] + board[planeWins[w][2][0]][planeWins[w][2][1]];
         if (Math.abs(linescore) == 3) {
-            gameEnds(linescore);
+            gameEnds(linescore, potentialWinCoords);
             return;
         }
         if (linescore == -3) {
-            gameEnds(linescore);
+            gameEnds(linescore, potentialWinCoords);
             return;
         }
     }
     // check to see if the board is filled up for a tie
     const isGridFull = () => board.every(row => !row.includes(0));
     if (isGridFull()) {
-        gameEnds(0);
+        gameEnds(0, potentialWinCoords);
     }
 }
 
-function gameEnds(state) {
+function gameEnds(state, winArray) {
     // disable clicking on canvas
     canvas.removeEventListener('click', handleClick);
     if (state==3) {
         drawWinnerText("X wins");
+        highlightCell(highlightColors[0], winArray[0][0], winArray[0][1]);
+        highlightCell(highlightColors[0], winArray[1][0], winArray[1][1]);
+        highlightCell(highlightColors[0], winArray[2][0], winArray[2][1]);
     }
     else if (state==-3) {
         drawWinnerText("O wins");
+        highlightCell(highlightColors[1], winArray[0][0], winArray[0][1]);
+        highlightCell(highlightColors[1], winArray[1][0], winArray[1][1]);
+        highlightCell(highlightColors[1], winArray[2][0], winArray[2][1]);
     }
     else if (state==0) {
         drawWinnerText("Tie");
@@ -209,11 +227,11 @@ function handleClick(event) {
     if (validateClickCoords(x,y) && isEmpty(bx,by)) {
         if (clickCount%2==0) {
             board[bx][by] = 1;
-            drawX("red", bx, by);
+            drawX(playerColors[0], bx, by);
         }
         else {
             board[bx][by] = -1;
-            drawO("blue", bx, by);
+            drawO(playerColors[1], bx, by);
         }
         findWin();
         clickCount++;
